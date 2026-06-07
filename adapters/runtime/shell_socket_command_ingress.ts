c5c459998d7547f8959c93c74e3c70481a849d7d
jakerslam <jakerslam@gmail.com>
2026-06-06 22:51:36 -0600

    refactor(gateway): retire shell socket shims

--- adapters/runtime/shell_socket_command_ingress.ts
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env tsx
-// Compatibility shim: canonical Gateway socket module lives under gateway/**.
-module.exports = require('../../gateway/runtime/sockets/shell_socket/shell_socket_command_ingress.ts');
